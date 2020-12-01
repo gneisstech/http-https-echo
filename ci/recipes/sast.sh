8cc40d267db2310e7dc81f7c9025eee439296355
mystic knight <techguru@byiq.com>
2020-11-30 20:20:06 -1000

    use bedrock as scope reference

--- ci/recipes/sast.sh
+++ ci/recipes/sast.sh
@@ -13,7 +13,7 @@ set -o pipefail
 # Environment Variables
 # ---------------------
 declare -rx BUILD_REPOSITORY_LOCALPATH="${BUILD_REPOSITORY_LOCALPATH:-.}"
-declare -rx IMAGENAME="${IMAGENAME:-cf-http-https-echo}"
+declare -rx IMAGENAME="${IMAGENAME:-br-http-https-echo}"
 
 # Arguments
 # ---------------------
